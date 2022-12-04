bdb53719c0dcfcfc4b3ab252943d9cd047510186
KDebowiec <karoldebowiec@gmail.com>
2022-12-04 19:32:05 +0100

    'dec'

--- rozszerzenie/funkcje/funkcje3.py
+++ rozszerzenie/funkcje/funkcje3.py
@@ -7,9 +7,9 @@ def fizz_buzz(number):
 
     if number % 3 == 0:
         print('Fizz')
-    if number % 5 == 0:
+    elif number % 5 == 0:
         print('Buzz')
-    if number % 3 == 0 and number % 5 == 0:
+    elif number % 3 == 0 and number % 5 == 0:
         print('FizzBuzz')
     else:
         print(number)
