691f904e491f1f218f02e4cd0eaabf3763e8bf2a
KDebowiec <karoldebowiec@gmail.com>
2022-12-08 21:10:35 +0100

    'fuctions_8'

--- rozszerzenie/funkcje/funkcje8.py
+++ rozszerzenie/funkcje/funkcje8.py
@@ -4,11 +4,18 @@
 # Podpowiedź:
 # Możesz wykorzystać funkcję abs w celu obliczania wartości bezwzględnej, np.
 # print(abs(-5)) # 5
-def one_list(**kwargs):
+# def one_list(**kwargs):
+def equation(**kwargs):
+    angle = abs(6 * kwargs['kwarg_2'] - 30 * (kwargs['kwarg_1'] + kwargs['kwarg_2'] / 60))
+    print(f'kąt między wskazówkami to {angle}')
 
 
 def main():
-
+    time = input('która jest godzina? podaj w formacie 12:00: ')
+    time_list = time.split(':')
+    hour = int(time_list[0])
+    minute = int(time_list[1])
+    equation(kwarg_1=hour, kwarg_2=minute)
 
 
 if __name__ == '__main__':
