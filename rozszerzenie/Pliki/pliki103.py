d7d9457806f3f2f6f4c64313a15fb48abb31c14f
Stefan Reszel <stefanreszel@outlook.com>
2023-01-16 18:04:15 +0100

    pliki103

--- rozszerzenie/Pliki/pliki103.py
+++ rozszerzenie/Pliki/pliki103.py
@@ -8,23 +8,91 @@
 file.readline() => zwraca linie, gdy koniec pliku, pusty string.
 petla while => dopuki nie skonczy sie plik
 wyciagać 3 linie z pliku
-
 """
 
-with open('pliki10_data.txt', 'r') as file:
-    lines = []
+# Karolowi wyszło 753
 
-    for i in range(3):
-        line = file.readline()
-        lines.append(line.split())
+lines_to_check = [None, None]
+amount_of_contrast_number = 0
 
-    print(lines)
+"""
+lines_to_check = [
+    [5, 6, 7, 8, 9],
+    [0, 1, 2, 3, 4],
+]
+"""
 
-    # while line:
-    #     print(line)
-    #     line = file.readline()
-    #
-[13, 150, 15]
+def check_lines(lines):
+    amount_of_contrast_number = 0
+    line_length = len(lines[0])
+
+    for i in range(line_length):
+        cursor = int(lines[0][i])
+
+        if i < line_length - 1:
+            is_next_contrast = abs(cursor - int(lines[0][i+1])) > 128
+        else:
+            is_next_contrast = False
+
+        if i > 0:
+            is_prev_contrast = abs(cursor - int(lines[0][i-1])) > 128
+        else:
+            is_prev_contrast = False
+
+        is_below_contrast = abs(cursor - int(lines[1][i])) > 128
+
+        if is_next_contrast or is_prev_contrast or is_below_contrast:
+            amount_of_contrast_number += 1
+
+        cursor = int(lines[1][i])
+
+        if i < line_length - 1:
+            is_next_contrast = abs(cursor - int(lines[1][i+1])) > 128
+        else:
+            is_next_contrast = False
+
+        if i > 0:
+            is_prev_contrast = abs(cursor - int(lines[1][i-1])) > 128
+        else:
+            is_prev_contrast = False
+
+        is_above_contrast = abs(cursor - int(lines[0][i])) > 128
+
+        if is_next_contrast or is_prev_contrast or is_above_contrast:
+            amount_of_contrast_number += 1
+
+    return amount_of_contrast_number
+
+def change_lines(file, present_lines):
+    present_lines.pop(0)
+
+    present_lines.insert(1, file.readline().split())
+
+    return present_lines
+
+
+with open("pliki10_data.txt", "r") as file:
+    for i in range(200):
+        lines_to_check = [file.readline().split(), file.readline().split()]
+
+        to_add = check_lines(lines_to_check)
+        amount_of_contrast_number += to_add
+        lines_to_check = change_lines(file, lines_to_check)
+
+
+print(amount_of_contrast_number)
+
+
+# while any(lines_to_check):
+#     with open("pliki10_data.txt", "r") as file:
+
+
+
+
+# with open("pliki10_data.txt", "r") as file:
+#     ...
+
+
 
 # file = open('pliki10_data.txt', 'r')
 # list_of_lines = []
@@ -71,3 +139,4 @@ with open('pliki10_data.txt', 'r') as file:
 # result = len(list_of_pairs)
 # print(f'wszystkich takich pikseli, dla których istnieje przynajmniej jeden kontrastujący z nim sąsiedni piksel jest'
 #       f' {result}')
+#
