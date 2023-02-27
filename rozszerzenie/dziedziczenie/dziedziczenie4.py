e93930fe83869e2b8d43ea49235312253b0f3e64
Carlositto <wiatrowskikarol20@gmail.com>
2023-02-27 12:57:43 +0100

    Przerobione zadanie z dziedziczenia (tylko pierwsza część)

--- rozszerzenie/dziedziczenie/dziedziczenie4.py
+++ rozszerzenie/dziedziczenie/dziedziczenie4.py
@@ -34,66 +34,60 @@
 #    1. SubManager_1 -> Menu (add_card, show_list)
 #    2. SubManager_2 -> Menu (add_card, show_list)
 #    3. Exit
-
-class Card:
-    def __init__(self):
-        self.list_of_cards = []
-
-    def add_card(self):
-        card = input('wpisz wizytówkę: ')
-        self.list_of_cards.append(card)
-
-
-class Note:
-    def __init__(self):
-        self.list_of_notes = []
-
-    def add_note(self):
-        note = input('wpisz notatkę: ')
-        self.list_of_notes.append(note)
-
+#
+#class Card:
+#    def __init__(self):
+#        self.list_of_cards = []
+#
+#    def add_card(self):
+#        card = input('wpisz wizytówkę: ')
+#        self.list_of_cards.append(card)
+#
+#
+#class Note:
+#    def __init__(self):
+#        self.list_of_notes = []
+#
+#
 
 class Menu:
-
-    def __init__(self):
+    def __init__(self, **kwargs):
+        for menu_choice, menu_item in kwargs.items():
+            setattr(self, menu_choice, menu_item)
         self.show()
-
-    def show(self):
-        print('1. Dodaj notatkę')
-        print('2. Dodaj Wizytówkę')
-        print('3. Wyświetl notatki')
-        print('4. Wyświetl Wizytówki')
-        print('5. Wyjdź')
         self.get_choice()
 
+    def show(self):
+        for menu_choice, menu_item in self.__dict__.items():
+            print(menu_choice, menu_item)
+
     def get_choice(self):
-        choice = int(input("wybierz z menu: "))
-        self.execute(self, choice)
+        user_input = input('Wybierz numer:')
+        print(f'Wybrałeś {user_input} czyli {getattr(self, user_input)}')
+        self.execute(int(user_input))
 
 
-class Manager:
-    def __init__(self):
-        super().__init__()
-        self.start()
-
-    def start(self):
-        Manager.show_menu(self)
-
-    def show_menu(self):
-        Menu()
+class Manager(Menu):
+    def __init__(self, menu):
+        super().__init__(**menu)
 
     def execute(self, choice):
-
         if choice == 1:
-            Note.add_note()
-        if choice == 2:
-            Card.add_card()
-        if choice == 3:
-            print(list_of_notes)
-        if choice == 4:
-            print(list_of_cards)
-        if choice == 5:
-            exit()
+            NoteManager(note_menu)
 
+class NoteManager(Manager):
+    def __init__(self, menu):
+        self.list_of_notes = []
+        super().__init__(menu)
 
-Manager()
+    def add_note(self):
+        note = input('Wpisz notatkę: ')
+        self.list_of_notes.append(note)
+
+    def execute(self, choice):
+        if choice == 1:
+            self.add_note()
+
+note_menu = {'1': 'Dodaj notatkę', '2': 'Wyświetl wszystko'}
+tmp_menu = {'1': 'Manager notatek'}
+Manager(tmp_menu)
