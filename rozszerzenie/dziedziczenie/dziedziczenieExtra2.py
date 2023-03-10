0d45c8d1eb83e4bf4b09850c86d77736bb1719b9
Carlositto <wiatrowskikarol20@gmail.com>
2023-03-10 23:25:14 +0100

    Nowy sposób na dodatkowe zadanie z dziedziczenia.

--- rozszerzenie/dziedziczenie/dziedziczenieExtra2.py
+++ rozszerzenie/dziedziczenie/dziedziczenieExtra2.py
@@ -1,98 +1,126 @@
-
 class Category:
+
     def __init__(self, category_name):
         self.category_name = category_name
 
-    def choose_category(self):
-        pass
+    def __str__(self):
+        return self.category_name
+
+    def get_sizes(self):
+        if self.category_name.lower() == 'buty':
+            return [size for size in range(37, 51)]
+        return ['XS', 'S', 'M', 'L', 'XL', 'XXL']
 
 
 class ShopItem(Category):
-    def __init__(self, category_name, name, size):
-        super().__init__(category_name)
+
+    def __init__(self, name, size):
         self.name = name
         self.size = size
 
+    def __str__(self):
+        return f'{ self.name } - { self.size } { (self.category_name) }'
+
 
 class ShopWorker:
-    def __init__(self, owner, first_name):
+
+    def __init__(self, login, owner=False):
+        self.login = login
         self.owner = owner
-        self.first_name = first_name
+
+    def __str__(self):
+        return self.login
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class ShopDataBase:
-    def __init__(self):
-        self.products = {}
-        self.clothes_sizes = ['S', 'M', 'L', 'XL', 'XXL']
-        self.shoes_sizes = [37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51]
-        self.user = None
-        self.show()
 
-    def log_in(self):
-        self.user = input('login: kim jesteś? ')
-        self.show()
+    def __init__(self, users, categories, logged_in_user=None, products={}):
+        self.users = users
+        self.logged_in_user = logged_in_user
+        self.products = products
+        self.categories = categories
+        self.show_menu()
 
-    def log_out(self):
-        self.user = None
-        print('wylogowałeś się!!!!!! ')
-        self.show()
-        return self.user
+    def show_menu(self):
+        # Domyślne menu
+        menu = {
+            '1': 'Lista produktów',
+            '2': 'Zaloguj',
+        }
+        if self.logged_in_user:
+            # Sprawdzenie czy ta wartość nie jest None
+            # Jeżeli ktoś się zalogował to na pewno to pracownik sklepu i może dodawać produkty i wylogować się
+            menu['2'] = 'Dodaj produkt'
+            menu['3'] = 'Wyloguj się'
+            if self.logged_in_user.owner:
+                # Jeżeli to owner to dodajemy do menu akcję usuwania produktu
+                # owner może być tylko True lub False
+                menu['4'] = 'Usuń produkt'
+        #  Na końcu jak już owarunkowane menu sie zbudowało to wyświetlamy
+        for number, text in menu.items():
+            print(f'{number}. {text}')
+        self.get_choice()
 
-    def show(self):
-        choice_from_menu = int(input('1: Lista produktów\n2: dodaj produkt\n3: zaloguj/wyloguj\n4: usuń produkt\nwybór: '))
-        if choice_from_menu == 1:
-            self.print_products()
-        elif choice_from_menu == 2:
-            self.add_product()
-        elif choice_from_menu == 3:
-            if self.user:
-                self.log_out()
-            else:
+    def get_choice(self):
+        user_input = input('Wybierz numer: ')
+        self.execute(user_input)
+
+    def execute(self, user_input):
+        if user_input == '1':
+            self.show_products()
+        if self.logged_in_user:
+            if user_input == '2':
+                self.add_product()
+            if user_input == '3':
+                self.logged_in_user = None
+                self.show_menu()
+            if user_input == '4':
+                self.delete_product()
+        else:  # <-- tutaj nie ma zalogowanego usera
+            if user_input == '2':
                 self.log_in()
-        elif choice_from_menu == 4:
-            self.delete_product()
+        # Te dwie linijki poniżej wykonają się jeżeli żaden z 'ifów' wyżej nie spełni się
+        self.show_menu()
 
     def add_product(self):
-        type_choice = int(input('typ produktu:\n1 - ciuchy\n2 - suplementy\n3 - buty\nwybierz: '))
-        if type_choice == 1:
-            category_name = 'ciuchy'
-            size = input('wybierz rozmiar S, M, L, Xl, XXl: ')
-            if size not in self.clothes_sizes:
-                print('Nie ma takiego rozmiaru typie')
-        elif type_choice == 2:
-            category_name = 'suplementy'
-            size = int(input('wybierz rozmiar od 37 do 51: '))
-            if size not in self.shoes_sizes:
-                print('Nie ma takiego rozmiaru typie')
-        elif type_choice == 3:
-            category_name = 'buty'
-            size = input('wybierz rozmiar S, M, L, Xl, XXl: ')
-            if size not in self.clothes_sizes:
-                print('Nie ma takiego rozmiaru typie')
-        name = input('podaj nazwę: ')
-        new_product = ShopItem(name, size, category_name)
-        id_ = id(new_product)
-        self.products.update({id_: {'nazwa': name, 'rozmiar': size, 'typ': category_name}})
-        self.show()
+        for category in self.categories:
+            print(category)
+        category_name = input('Wybierz kategorię: ').lower()
+        category = self.categories[category_name]
+        name = input('Podaj nazwę produktu: ')
+        size = input(f'Wybierz rozmiar produktu {category.get_sizes()}: ')
+        new_product = ShopItem(name, size)
+        new_product.category_name = category.category_name
+        product_id = id(new_product)
+        self.products[product_id] = new_product
+
+    def log_in(self):
+        user_input = input(f'Dostępni użytkownicy to {self.users}. Podaj login: ')
+        for user in self.users:
+            if user_input == user.login:
+                self.logged_in_user = user
+        self.show_menu()
+
+    def show_products(self):
+        for product_id, product in self.products.items():
+            print(product_id, product.name, product.size, product.category_name)
 
     def delete_product(self):
-        if self.user == owner.first_name:
-            self.print_products()
-            chosen = int(input('podaj id produktu który chcesz usunąć: '))
-            self.products.pop(chosen)
-            self.show()
-        else:
-            print('nie masz uprawnień!')
-            self.show()
-
-    def print_products(self):
-        for i in self.products.items():
-            print(i)
-        self.show()
+        self.show_products()
+        to_delete = input('Podaj id produktu do usunięcia: ')
+        if int(to_delete) in self.products:
+            self.products.pop(int(to_delete))
+        self.show_menu()
 
 
-owner = ShopWorker(owner=True, first_name='Karol')
-worker = ShopWorker(owner=False, first_name='Paweł')
-
-
-ShopDataBase()
+karol = ShopWorker('karol', True)
+pawel = ShopWorker('pawel')
+users = [karol, pawel]
+shoes = Category('buty')
+clothes = Category('ciuchy')
+supplements = Category('suplementy')
+categories = {shoes.category_name: shoes, clothes.category_name: clothes, supplements.category_name: supplements}
+ShopDataBase(users=users, categories=categories)
